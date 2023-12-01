9f2e9348c7ed6a63066f5cb949da29de797500e9
Fudgey <fudgebucket27@gmail.com>
2023-12-01 15:15:09 +1000

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var sum = 0;
+foreach (var line in lines)
+{
+    var nums = "";
+    var digits = line.Where(Char.IsDigit).ToArray();
+    nums += digits[0].ToString() + digits[digits.Length - 1].ToString();
+    sum += Int32.Parse(nums);
+}
+Console.WriteLine(sum);
