3eb40a1d70a747805d4ce3435a17a350b07ca33a
Fudgey <fudgebucket27@gmail.com>
2023-12-04 09:45:39 +1000

    done

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,48 +1,56 @@
-﻿using Microsoft.VisualBasic;
+﻿var lines = File.ReadAllLines("input.txt");
 
-var lines = File.ReadAllLines("input.txt");
+//Part 1
 var sum = 0;
-//foreach (var line in lines)
-//{
-//    var nums = "";
-//    var digits = line.Where(Char.IsDigit).ToArray();
-//    nums += digits[0].ToString() + digits[digits.Length - 1].ToString();
-//    sum += Int32.Parse(nums);
-//}
-//Console.WriteLine(sum);
+foreach (var line in lines)
+{
+    var nums = "";
+    var digits = line.Where(Char.IsDigit).ToArray();
+    nums += digits[0].ToString() + digits[digits.Length - 1].ToString();
+    sum += Int32.Parse(nums);
+}
+Console.WriteLine(sum);
 
+//Part 2
 sum = 0;
 var numberStrings = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 var numberIntStrings = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 var numberInts = new List<int> {1,2,3,4,5,6,7,8,9};
 foreach (var line in lines)
 {
-    Console.WriteLine("Current Line:" + line);
-    var maxNumberStringIndex = -1;
-    var minNumberStringIndex = -1;
-    var minNumberStringInt = -1;
-    var maxNumberStringInt = -1;
-    int count = 0;
-    for(int i=0; i < numberStrings.Count(); i++) 
+    Dictionary<int, int> numbersFound = new Dictionary<int, int>(); //key = index, value is number
+    for (int i = 0; i < numberStrings.Count(); i++)
     {
         var first = line.IndexOf(numberStrings[i]);
         var last = line.LastIndexOf(numberStrings[i]);
-        Console.WriteLine($"Current num: {numberStrings[i]}, First: {first}, Last: {last}");
-        if(first <= last)
+        if (first != -1 && !numbersFound.ContainsKey(first))
         {
-            minNumberStringIndex = first;
-            minNumberStringInt = numberInts[i];
+            numbersFound.Add(first, numberInts[i]);
         }
 
-        if(last >= first)
+        if (last != -1 && !numbersFound.ContainsKey(last))
         {
-            maxNumberStringIndex = last;
-            maxNumberStringInt = numberInts[i];
+            numbersFound.Add(last, numberInts[i]);
         }
-        Console.WriteLine($"{minNumberStringInt},{maxNumberStringInt}");
     }
-    Console.WriteLine($"FINAL {minNumberStringInt}{maxNumberStringInt}");
+    for (int i = 0; i < numberIntStrings.Count(); i++)
+    {
+        var first = line.IndexOf(numberIntStrings[i]);
+        var last = line.LastIndexOf(numberIntStrings[i]);
+        if (first != -1 && !numbersFound.ContainsKey(first))
+        {
+            numbersFound.Add(first, numberInts[i]);
+        }
 
+        if (last != -1 && !numbersFound.ContainsKey(last))
+        {
+            numbersFound.Add(last, numberInts[i]);
+        }
+    }
+    var minNumberKey = numbersFound.Keys.Min();
+    var maxNumberKey = numbersFound.Keys.Max();
+    var nums = numbersFound[minNumberKey].ToString() + numbersFound[maxNumberKey].ToString();
+    sum += Int32.Parse(nums);
 }
 
 Console.WriteLine(sum);
