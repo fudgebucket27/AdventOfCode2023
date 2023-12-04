b44665a6dd2cc8ed38c0cdf11cc9cfbd5e3ac537
Fudgey <fudgebucket27@gmail.com>
2023-12-04 16:18:59 +1000

    getting closer

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -29,3 +29,49 @@ foreach(var line in lines)
 }
 Console.WriteLine(points);
 
+points = 0;
+var cardNumber = 0;
+Dictionary<int,int> cards = new Dictionary<int,int>();
+foreach (var line in lines)
+{
+    cardNumber++;
+    var firstMatch = false;
+    var matches = 0;
+    var game = line.Split(':')[1].Split('|');
+    var winningNumbers = game[0].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToList();
+    var numbersTocheck = game[1].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+    foreach (var number in numbersTocheck)
+    {
+        foreach (var winningNumber in winningNumbers)
+        {
+            if (winningNumber == number)
+            {
+                matches += 1;
+            }
+        }
+    }
+
+    if(matches == 0) //if no matches count original 
+    {
+        matches = 1;
+    }
+
+    Console.WriteLine($"Card {cardNumber} Results: ");
+    for (int i = 0; i <= matches; i++)
+    {
+        int currentCard = cardNumber + i;
+
+        if (!cards.ContainsKey(currentCard))
+        {
+            cards.Add(currentCard, 1); // or 1, if you want to count the card itself regardless of matches
+            Console.WriteLine($"Adding original card: {currentCard}");
+        }
+        else
+        {
+            cards[currentCard] += 1; // or increment by 1, depending on your requirements
+            Console.WriteLine($"Updating card: {currentCard}");
+        }
+    }
+}
+Console.WriteLine(cards.Sum(x=> x.Value));
