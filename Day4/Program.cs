919f4200cb236115d0e8bbd2839fa4970089cd23
Fudgey <fudgebucket27@gmail.com>
2023-12-04 15:39:26 +1000

    blah

---
+++ Day4/Program.cs
@@ -0,0 +1,8 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var points = 0;
+foreach(var line in lines)
+{
+    var game = line.Split(':')[1].Split('|');
+    var winningNumbers = game[0].Trim().Split(' ').Where(x=> !string.IsNullOrEmpty(x) ).ToList();
+    var numbersTocheck = game[1].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToList();
+}
