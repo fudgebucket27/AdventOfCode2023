d7cf2b4f97a7bc84fa951491e10347e57d803b80
Fudgey <fudgebucket27@gmail.com>
2023-12-04 11:01:02 +1000

    bra

---
+++ Day3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Day3;
+
+var lines = File.ReadAllLines("input.txt");
+var schematics = new List<Schematic>();
+foreach(var line in lines)
+{
+    foreach(var character in line)
+    {
+
+    }
+}
---
+++ Day3/Schematic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Schematic
+    {
+        int Number { get; set; }
+        bool isSymbol { get; set; } = false;
+        int west { get; set; }
+        int east { get; set; }  
+        int south { get; set; }
+        int north { get; set; }
+        int southwest { get; set; }
+        int southeast { get; set; }
+        int northwest { get; set; }
+        int northeast { get; set; }     
+    }
+}
