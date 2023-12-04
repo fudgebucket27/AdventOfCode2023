d96c16e3daa6dff42e73b9114373cde07cc43979
Fudgey <fudgebucket27@gmail.com>
2023-12-04 15:49:28 +1000

    day 4 part 1 done

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,8 +1,31 @@
 ﻿var lines = File.ReadAllLines("input.txt");
-var points = 0;
+
+//part 1
+double points = 0;
 foreach(var line in lines)
 {
+    var firstMatch = false;
+    var matches = 0;
     var game = line.Split(':')[1].Split('|');
     var winningNumbers = game[0].Trim().Split(' ').Where(x=> !string.IsNullOrEmpty(x) ).ToList();
     var numbersTocheck = game[1].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+    foreach(var number in numbersTocheck)
+    {
+       foreach(var winningNumber in winningNumbers)
+        {
+            if(winningNumber == number)
+            {
+                matches += 1;
+            }
+        }
+    }
+
+    if(matches > 0)
+    {
+        var power = matches - (double)1;
+        points += Math.Pow((double)2, power);
+    }
 }
+Console.WriteLine(points);
+
