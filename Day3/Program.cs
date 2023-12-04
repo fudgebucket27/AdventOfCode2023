4ac03d3f918f01b2d7a26486c6011b9c316fc322
Fudgey <fudgebucket27@gmail.com>
2023-12-04 14:46:28 +1000

    kinda working

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -5,38 +5,40 @@ using System.Numerics;
 var lines = File.ReadAllLines("input.txt");
 var schematics = new List<Schematic>();
 
-int x = 0;
-int y= 0;
-
 int lineCount = 0;
 foreach (var line in lines)
 {
     lineCount++;
+    int x = 0; // Reset x for each line
     string scanned = "";
     Schematic? schematic = null;
     foreach (var character in line)
     {
-        if(character == '.')
+        if (character == '.')
         {
             scanned = "";
-            if(schematic != null)
+            if (schematic != null)
             {
                 schematics.Add(schematic);
                 schematic = null;
             }
         }
-        else if(character != '.' && char.IsDigit(character))
+        else
         {
-            if (scanned.Count() == 0)
+            if (scanned.Count() == 0 || !char.IsDigit(character))
             {
-                scanned += character;
+                if (scanned.Count() > 0)
+                {
+                    schematics.Add(schematic);
+                }
+                scanned = character.ToString();
                 schematic = new Schematic()
                 {
                     LineNumber = lineCount,
-                    IsSymbol = false,
+                    IsSymbol = !char.IsDigit(character),
                     Text = scanned,
                     minX = x,
-                    minY = y
+                    minY = lineCount // Using lineCount as y-coordinate
                 };
             }
             else
@@ -44,71 +46,72 @@ foreach (var line in lines)
                 scanned += character;
                 schematic.Text = scanned;
             }
-       }else if (character != '.' && !char.IsDigit(character))
-       {
-            if(scanned.Count() > 0)
-            {
-                schematic = new Schematic()
-                {
-                    LineNumber = lineCount,
-                    IsSymbol = false,
-                    Text = scanned,
-                    minX = x,
-                    minY = y
-                };
-                schematics.Add(schematic);
-            }
-            scanned = "";
-            scanned += character;
-            schematic = new Schematic()
-            {
-                LineNumber = lineCount,
-                IsSymbol = true,
-                Text = scanned,
-                minX = x,
-                minY = y
-            };     
         }
         x++;
     }
-    y++;
-}
-
-foreach(var currentSchematic in schematics)
-{
-    Console.WriteLine("Schematic:" + currentSchematic.Text + ", Line:" +currentSchematic.LineNumber + ",X:" + currentSchematic.minX + ",Y:" + currentSchematic.minY);
-    for(int i= 0; i < currentSchematic.Text.Length; i++)
+    if (scanned.Count() > 0)
     {
-        int westX = currentSchematic.minX - (1+i);
-        int westY = currentSchematic.minY;
-        int eastX = currentSchematic.minX + (1+i);
-        int eastY = currentSchematic.minY;
-        int northX = currentSchematic.minX;
-        int northY = currentSchematic.minY + (1+i);
-        int southX = currentSchematic.minX;
-        int southY = currentSchematic.minY - (1+i);
-        int southWestX = currentSchematic.minX - (1 + i);
-        int southWestY = currentSchematic.minY - (1 + i);
-        int southEastX = currentSchematic.minX + (1 + i);
-        int southEastY = currentSchematic.minY - (1 + i);
-        int northWestX = currentSchematic.minX - (1 + i);
-        int northWestY = currentSchematic.minY + (1 + i);
-        int northEastX = currentSchematic.minX + (1 + i);
-        int northEastY = currentSchematic.minY + (1 + i);
+        schematics.Add(schematic);
+    }
+}
+Dictionary<string, int>  matched = new Dictionary<string, int>();
+foreach (var currentSchematic in schematics.Where(x => x.IsSymbol == false))
+{
+    Console.WriteLine("Schematic:" + currentSchematic.Text + ", Line:" + currentSchematic.LineNumber + ", X:" + currentSchematic.minX + ", Y:" + currentSchematic.minY);
 
-        foreach (var toMatch in schematics.Where(x=> x.IsSymbol == true && x.Text != currentSchematic.Text))
+    for (int i = 0; i < currentSchematic.Text.Length; i++)
+    {
+        var currentPosX = currentSchematic.minX + i;
+        var currentPosY = currentSchematic.minY;
+
+        int westX = currentPosX - 1;
+        int westY = currentPosY;
+
+        int eastX = currentPosX + 1;
+        int eastY = currentPosY;
+
+        int northX = currentPosX;
+        int northY = currentPosY - 1;
+
+        int southX = currentPosX;
+        int southY = currentPosY + 1;
+
+        int southWestX = currentPosX - 1;
+        int southWestY = currentPosY + 1;
+
+        int southEastX = currentPosX + 1;
+        int southEastY = currentPosY + 1;
+
+        int northWestX = currentPosX - 1;
+        int northWestY = currentPosY - 1;
+
+        int northEastX = currentPosX + 1;
+        int northEastY = currentPosY - 1;
+
+        foreach (var toMatch in schematics.Where(x => x.IsSymbol == true))
         {
-            if ((toMatch.minX == westX && toMatch.minY == westY) ||
-                 (toMatch.minX == eastX && toMatch.minY == eastY) ||
-                 (toMatch.minX == northX && toMatch.minY == northY) ||
-                 (toMatch.minX == southX && toMatch.minY == southY) ||
-                 (toMatch.minX == southWestX && toMatch.minY == southWestY) ||
-                 (toMatch.minX == southEastX && toMatch.minY == southEastY) ||
-                 (toMatch.minX == northWestX && toMatch.minY == northWestY) ||
-                 (toMatch.minX == northEastX && toMatch.minY == northEastY))
+            if ((toMatch.minX == westX && toMatch.LineNumber == westY) ||
+                (toMatch.minX == eastX && toMatch.LineNumber == eastY) ||
+                (toMatch.minX == northX && toMatch.LineNumber == northY) ||
+                (toMatch.minX == southX && toMatch.LineNumber == southY) ||
+                (toMatch.minX == southWestX && toMatch.LineNumber == southWestY) ||
+                (toMatch.minX == southEastX && toMatch.LineNumber == southEastY) ||
+                (toMatch.minX == northWestX && toMatch.LineNumber == northWestY) ||
+                (toMatch.minX == northEastX && toMatch.LineNumber == northEastY))
             {
-                Console.WriteLine($"Match found: {currentSchematic.Text} at ({currentSchematic.minX}, {currentSchematic.minY}) matches with {toMatch.Text} at ({toMatch.minX}, {toMatch.minY})");
+                var key = $"{currentSchematic.minX},{currentSchematic.minY}";
+                if(!matched.ContainsKey(key))
+                {
+                    matched.Add(key, Int32.Parse(currentSchematic.Text));
+                }
+                Console.WriteLine($"Match found: {currentSchematic.Text} at ({currentSchematic.minX}, {currentSchematic.LineNumber}) matches with {toMatch.Text} at ({toMatch.minX}, {toMatch.LineNumber})");
             }
         }
     }
 }
+var sum = 0;
+foreach(var match in matched)
+{
+    sum += match.Value;
+}
+Console.WriteLine(sum);
