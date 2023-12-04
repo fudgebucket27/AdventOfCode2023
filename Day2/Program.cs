69059137263de91b6290b823dd02ae6474c8c475
Fudgey <fudgebucket27@gmail.com>
2023-12-04 10:44:00 +1000

    day 2 part 2 done!

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Drawing;
 
 var lines = File.ReadAllLines("input.txt");
+
+//Part 1
 var sum = 0;
 foreach (var game in lines)
 {
@@ -47,5 +49,42 @@ foreach (var game in lines)
     {
         sum += gameId;
     }
+}
+Console.WriteLine(sum);
+sum = 0;
+//Part 2
+foreach (var game in lines)
+{
+    int gameId = Int32.Parse(game.Split(':')[0].Split(' ')[1]);
+    string[] gameColourSets = game.Split(":")[1].Split(';');
+    Dictionary<string, int> gameColourSetAmounts = new Dictionary<string, int>();
+    foreach (var gameSet in gameColourSets)
+    {
+        string[] colourSets = gameSet.Split(',');
+        foreach (var colourSet in colourSets)
+        {
+            var data = colourSet.Split(" ");
+            var dataAmount = Int32.Parse(data[1]);
+            var colourName = data[2];
+            if (gameColourSetAmounts.ContainsKey(colourName))
+            {
+                if (dataAmount > gameColourSetAmounts[colourName])
+                {
+                    gameColourSetAmounts[colourName] = dataAmount;
+                }
+            }
+            else
+            {
+                gameColourSetAmounts.Add(colourName, dataAmount);
+            }
+        }
+    }
+
+    var powers = 1;
+    foreach(var gameColourSet in gameColourSetAmounts)
+    {
+        powers *= gameColourSet.Value;
+    }
+    sum += powers;
 }
 Console.WriteLine(sum);
