6a6b5c0c3822aad5413e1603690d35077f9d8599
Fudgey <fudgebucket27@gmail.com>
2023-12-05 14:33:07 +1000

    bra

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -5,26 +5,26 @@ var lines = File.ReadAllLines("input.txt");
 
 //part 1
 double points = 0;
-foreach(var line in lines)
+foreach (var line in lines)
 {
     var firstMatch = false;
     var matches = 0;
     var game = line.Split(':')[1].Split('|');
-    var winningNumbers = game[0].Trim().Split(' ').Where(x=> !string.IsNullOrEmpty(x) ).ToList();
+    var winningNumbers = game[0].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToList();
     var numbersTocheck = game[1].Trim().Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToList();
 
-    foreach(var number in numbersTocheck)
+    foreach (var number in numbersTocheck)
     {
-       foreach(var winningNumber in winningNumbers)
+        foreach (var winningNumber in winningNumbers)
         {
-            if(winningNumber == number)
+            if (winningNumber == number)
             {
                 matches += 1;
             }
         }
     }
 
-    if(matches > 0)
+    if (matches > 0)
     {
         var power = matches - (double)1;
         points += Math.Pow((double)2, power);
@@ -34,7 +34,7 @@ Console.WriteLine(points);
 
 points = 0;
 var cardNumber = 0;
-Dictionary<int,int> cards = new Dictionary<int,int>();
+Dictionary<int, int> cards = new Dictionary<int, int>();
 foreach (var line in lines)
 {
     cardNumber++;
@@ -53,14 +53,14 @@ foreach (var line in lines)
             }
         }
     }
-    
+
     matches += 1;
 
     var key = cardNumber;
     if (!cards.ContainsKey(key))
     {
         cards.Add(key, 1); // or 1, if you want to count the card itself regardless of matches
-        
+
     }
     else
     {
@@ -72,10 +72,10 @@ foreach (var line in lines)
     for (int i = 1; i < matches; i++)
     {
         int currentCard = cardNumber + i;
-        
+
         var currentKey = currentCard;
 
-        if(cardNumber == 1) 
+        if (cardNumber == 1)
         {
             if (!cards.ContainsKey(currentKey))
             {
@@ -90,25 +90,24 @@ foreach (var line in lines)
         }
         else
         {
-            for(int j = 1; j < matches; j++) 
+
+            if (!cards.ContainsKey(currentKey))
             {
-                if (!cards.ContainsKey(currentKey))
-                {
-                    cards.Add(currentKey, 1); // or 1, if you want to count the card itself regardless of matches
-                    Console.WriteLine($"Copying card: {currentCard}, Count={cards[currentKey]}");
-                }
-                else if (cards.ContainsKey(currentKey))
-                {
-                    cards[currentKey] += 1; // or 1, i
-                    Console.WriteLine($"Copying card: {currentCard}, Count={cards[currentKey]}");
-                }
+                cards.Add(currentKey, 1); // or 1, if you want to count the card itself regardless of matches
+                Console.WriteLine($"Copying card 1: {currentCard}, Count={cards[currentKey]}");
+            }
+            else if (cards.ContainsKey(currentKey))
+            {
+                int currentCount = cards[currentKey];
+                cards[currentKey] += currentCount; // or 1, i
+                Console.WriteLine($"Copying card 2: {currentCard}, Count={cards[currentKey]}");
             }
         }
     }
 }
-foreach(var card in cards)
+foreach (var card in cards)
 {
     Console.WriteLine($"Cards: {card.Key}, Count: {card.Value}");
 }
 
-Console.WriteLine("Sum:"+cards.Sum(x=> x.Value));
+Console.WriteLine("Sum:" + cards.Sum(x => x.Value));
