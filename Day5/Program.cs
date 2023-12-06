faeb2cdb408e69c86ed32d740050d80b3029e7d8
Fudgey <fudgebucket27@gmail.com>
2023-12-06 10:49:49 +1000

    bra

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -43,36 +43,39 @@ class Program
 
 
         //Part 2
+        Dictionary<string, SortedSet<Range>> efficientMaps = new Dictionary<string, SortedSet<Range>>();
+        foreach (var mapEntry in maps)
+        {
+            var sortedRanges = new SortedSet<Range>(new RangeComparer());
+            foreach (var line in mapEntry.Value)
+            {
+                sortedRanges.Add(new Range(line[1], line[1] + line[2], line[0]));
+            }
+            efficientMaps.Add(mapEntry.Key, sortedRanges);
+        }
         List<long> part2Result = new List<long>();
         for (int i = 0; i < seeds.Count; i += 2)
         {
             long startSeed = seeds[i];
-            long endSeed = seeds[i + 1];
+            long rangeLength = seeds[i + 1];
 
-            for (long seed = startSeed; seed <= endSeed; seed++)
+            for (long seed = startSeed; seed < startSeed + rangeLength; seed++)
             {
                 long conversionResult = seed;
-                foreach (var map in maps)
+                foreach (var map in efficientMaps)
                 {
-                    foreach (var line in map.Value)
+                    var range = map.Value.FirstOrDefault(r => r.Contains(conversionResult));
+                    if (range != null)
                     {
-                        var destinationRangeStart = line[0];
-                        var sourceRangeStart = line[1];
-                        var sourceRangeLength = line[2];
-                        var sourceRangeEnd = sourceRangeStart + sourceRangeLength;
-
-                        if (conversionResult >= sourceRangeStart && conversionResult <= sourceRangeEnd)
-                        {
-                            conversionResult = destinationRangeStart + (conversionResult - sourceRangeStart);
-                            break; 
-                        }
-
+                        conversionResult = range.DestinationStart + (conversionResult - range.Start);
+                        break;
                     }
                 }
                 part2Result.Add(conversionResult);
             }
         }
         Console.WriteLine($"Part 2: Lowest Location number: {part2Result.Min()}");
+
     }
 
     static List<long> ExtractSeeds(string line)
---
+++ Day5/Range.cs
@@ -0,0 +1,27 @@
+﻿class Range
+{
+    public long Start { get; }
+    public long End { get; }
+    public long DestinationStart { get; }
+
+    public Range(long start, long end, long destinationStart)
+    {
+        Start = start;
+        End = end;
+        DestinationStart = destinationStart;
+    }
+
+    public bool Contains(long value) => value >= Start && value <= End;
+}
+
+class RangeComparer : IComparer<Range>
+{
+    public int Compare(Range x, Range y)
+    {
+        if (x.Start > y.End)
+            return 1;
+        if (y.Start > x.End)
+            return -1;
+        return 0;
+    }
+}
