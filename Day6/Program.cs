0ac1f0c9fc867b0006cfeb21fc6d1394b38be72a
Fudgey <fudgebucket27@gmail.com>
2023-12-06 16:32:09 +1000

    add stopwatch

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 //Part 1
+using System.Diagnostics;
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
 var times = new List<long> { 55, 82, 64, 90 };
 var distances = new List<long> { 246, 1441, 1012, 1111 };
 
@@ -26,9 +30,13 @@ for (int i = 0; i < distances.Count; i++)
     var wins = finalResults[i].Where(x => x > distances[i]).Count();
     waysToWin.Add(wins);
 }
-Console.WriteLine(waysToWin.Aggregate((a, x) => a * x));
+var winAmount = waysToWin.Aggregate((a, x) => a * x);
+sw.Stop();
+Console.WriteLine($"Ways to win: {winAmount}, Elapsed time: {sw.ElapsedMilliseconds} ms");
 
 //Part 2
+Stopwatch sw2  =new Stopwatch();
+sw2.Start();
 var part2Times = new List<long> { 55826490 };
 var part2Distances = new List<long> { 246144110121111 };
 
@@ -55,4 +63,5 @@ for (int i = 0; i < part2Distances.Count; i++)
     var wins = part2FinalResults[i].Where(x => x > part2Distances[i]).Count();
     part2WaysToWin.Add(wins);
 }
-Console.WriteLine(part2WaysToWin.Aggregate((a, x) => a * x));
+var win2Amount = part2WaysToWin.Aggregate((a, x) => a * x);
+Console.WriteLine($"Ways to win: {win2Amount}, Elapsed time: {sw2.ElapsedMilliseconds} ms");
