83dab06553ac72195f88a868d33917a7da6a2edd
Fudgey <fudgebucket27@gmail.com>
2023-12-06 15:28:24 +1000

    day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,11 @@
+﻿var times = new List<long> { 7, 15, 30 };
+var distances = new List<long> { 9, 40, 200 };
+
+foreach(var time in times)
+{
+    Console.WriteLine("Time": + time);
+    for(long i=0; i < time; i++)
+    {
+        
+    }
+}
