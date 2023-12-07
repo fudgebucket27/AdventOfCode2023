3f2efd7f0d61f300136982ed12cb534a119d0eca
Fudgey <fudgebucket27@gmail.com>
2023-12-07 16:07:35 +1000

    blah

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,37 +1,29 @@
 ﻿var input = File.ReadAllLines("input.txt");
 
-var strengths = new Dictionary<string, int>()
+var strengths = new Dictionary<char, int>()
 {
-    { "A", 14 },
-    { "K", 13 },
-    { "Q", 12 },
-    { "J", 11 },
-    { "T", 10 },
-    { "9", 9 },
-    { "8", 8 },
-    { "7", 7 },
-    { "6", 6 },
-    { "5", 5 },
-    { "4", 4 },
-    { "3", 3 },
-    { "2", 2 },
+    { 'A', 14 },
+    { 'K', 13 },
+    { 'Q', 12 },
+    { 'J', 11 },
+    { 'T', 10 },
+    { '9', 9 },
+    { '8', 8 },
+    { '7', 7 },
+    { '6', 6 },
+    { '5', 5 },
+    { '4', 4 },
+    { '3', 3 },
+    { '2', 2 },
 };
 
-List<Tuple<string, int>> hands = new List<Tuple<string, int>>();
+List<(string, int, int)> hands = new List<(string, int, int)>();
 foreach (var line in input)
 {
     var parts = line.Split(' ');
-    var handValues = Tuple.Create(parts[0], Int32.Parse(parts[1]));
-    hands.Add(handValues);
-}
-
-foreach (var hand in hands)
-{
-    var cards = hand.Item1;
-    var bid = hand.Item2;
-    var groups = cards.GroupBy(c => c)
-                         .Select(group => new { Label = group.Key, Count = group.Count() })
-                         .ToList();
+    var groups = parts[0].GroupBy(c => c)
+                     .Select(group => new { Label = group.Key, Count = group.Count() })
+                     .ToList();
 
     bool isFiveOfAKind = groups.Any(g => g.Count == 5);
     bool isFourOfAKind = groups.Any(g => g.Count == 4);
@@ -40,4 +32,45 @@ foreach (var hand in hands)
     bool isTwoPair = groups.Count(g => g.Count == 2) == 2;
     bool isOnePair = groups.Count(g => g.Count == 2) == 1 && groups.Count(g => g.Count == 1) == 3;
     bool isHighCard = groups.All(g => g.Count == 1);
+
+    var handScore = 0;
+    if (isFiveOfAKind)
+    {
+        handScore += 7;
+    }
+    else if (isFourOfAKind)
+    {
+        handScore += 6;
+    }
+    else if (isFullHouse)
+    {
+        handScore += 5;
+    }
+    else if (isThreeOfAKind)
+    {
+        handScore += 4;
+    }
+    else if (isTwoPair)
+    {
+        handScore += 3;
+    }
+    else if (isOnePair)
+    {
+        handScore += 2;
+    }
+    else if (isHighCard)
+    {
+        handScore += 1;
+
+    }
+
+    foreach (var card in parts[0])
+    {
+        handScore += strengths[card];
+    }
+
+    hands.Add((parts[0], Int32.Parse(parts[1]), handScore));
 }
+
+hands.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+Console.WriteLine("END");
