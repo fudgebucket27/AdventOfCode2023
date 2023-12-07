1476fa95c7d74902bf1fbe6081cf076d9b12af8c
Fudgey <fudgebucket27@gmail.com>
2023-12-07 15:09:30 +1000

    boiler plate

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt");
+
+List<Tuple<string, int>> hands = new List<Tuple<string, int>>();
+foreach(var line in input)
+{
+    var parts = line.Split(' ');
+    var handValues = Tuple.Create(parts[0], Int32.Parse(parts[1]));
+    hands.Add(handValues);
+}
+Console.WriteLine("BRA");
